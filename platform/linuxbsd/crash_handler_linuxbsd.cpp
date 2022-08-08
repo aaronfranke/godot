301d309444edd77ebabd56b6934b93e929e03409
Aaron Franke <arnfranke@yahoo.com>
2022-08-08 14:37:55 -0500

    Add a method to print the stack trace

--- platform/linuxbsd/crash_handler_linuxbsd.cpp
+++ platform/linuxbsd/crash_handler_linuxbsd.cpp
@@ -30,6 +30,8 @@
 
 #include "crash_handler_linuxbsd.h"
 
+#include "stack_trace_linuxbsd.h"
+
 #include "core/config/project_settings.h"
 #include "core/io/file_access.h"
 #include "core/object/script_language.h"
@@ -37,6 +39,7 @@
 #include "core/os/os.h"
 #include "core/string/print_string.h"
 #include "core/version.h"
+#include "drivers/unix/stack_trace_unix.h"
 #include "main/main.h"
 
 #ifndef DEBUG_ENABLED
@@ -44,39 +47,10 @@
 #endif
 
 #ifdef CRASH_HANDLER_ENABLED
-#include <cxxabi.h>
 #include <dlfcn.h>
-#include <execinfo.h>
-#include <link.h>
 
 #include <csignal>
-#include <cstdio>
 #include <cstdlib>
-
-inline String find_addr2line_executable() {
-	List<String> args;
-	args.push_back("--version");
-	String output;
-	OS *os = OS::get_singleton();
-	int ret = 0;
-	Error err = OK;
-	// First, check for addr2line in the home directory's cargo bin.
-	if (os->has_environment("HOME")) {
-		// Faster implementation from gimli-rs/addr2line.
-		const String cargo_addr2line = os->get_environment("HOME").path_join(String("/.cargo/bin/addr2line"));
-		err = os->execute(cargo_addr2line, args, &output, &ret);
-		if (err == OK && ret == 0) {
-			return cargo_addr2line;
-		}
-	}
-	// Otherwise, check for llvm-addr2line.
-	err = os->execute(String("llvm-addr2line"), args, &output, &ret);
-	if (err == OK && ret == 0) {
-		return String("llvm-addr2line");
-	}
-	// Fallback guess if none of the above returned a definitive result.
-	return String("addr2line");
-}
 
 static void handle_crash(int sig) {
 	signal(SIGSEGV, SIG_DFL);
@@ -91,8 +65,11 @@ static void handle_crash(int sig) {
 		std::_Exit(0);
 	}
 
-	void *bt_buffer[256];
-	size_t size = backtrace(bt_buffer, 256);
+	void *bt_buffer[StackTraceUnix::MAX_FRAMES];
+	int size = backtrace(bt_buffer, StackTraceUnix::MAX_FRAMES);
+	if (size <= 0) {
+		abort();
+	}
 	String _execpath = OS::get_singleton()->get_executable_path();
 
 	if (FileAccess::exists(_execpath + ".debugsymbols")) {
@@ -120,73 +97,34 @@ static void handle_crash(int sig) {
 		print_error(vformat("Engine version: %s (%s)", GODOT_VERSION_FULL_NAME, GODOT_VERSION_HASH));
 	}
 	print_error(vformat("Dumping the backtrace. %s", msg));
-	char **strings = backtrace_symbols(bt_buffer, size);
-	// PIE executable relocation, zero for non-PIE executables
-#ifdef __GLIBC__
-	// This is a glibc only thing apparently.
-	uintptr_t relocation = _r_debug.r_map->l_addr;
-#else
-	// Non glibc systems apparently don't give PIE relocation info.
-	uintptr_t relocation = 0;
-#endif //__GLIBC__
+	const Vector<String> symbols = StackTraceUnix::collect_symbol_strings(bt_buffer, size);
+	const uintptr_t relocation = StackTraceLinuxBSD::get_relocation_offset();
 
 	print_error(vformat("Load address: %x\n", (uint64_t)relocation));
 
-	if (strings) {
-		int ret;
-		const String exe_name = find_addr2line_executable();
+	const String addr2line_exe = StackTraceLinuxBSD::find_addr2line_executable();
+	const Vector<uint64_t> addresses = StackTraceUnix::collect_addresses(bt_buffer, size);
+	const Vector<String> addr2line_results = StackTraceLinuxBSD::symbolize_with_addr2line(OS::get_singleton(), addr2line_exe, addresses, relocation, _execpath);
 
-		List<String> args;
-		for (size_t i = 0; i < size; i++) {
-			char str[1024];
-			snprintf(str, 1024, "%p", (void *)((uintptr_t)bt_buffer[i] - relocation));
-			args.push_back(str);
+	if (!addr2line_results.is_empty()) {
+		// addr2line with -f -p -C gives "function at file:line" per frame, use it directly.
+		for (int i = 1; i < size; i++) {
+			const String a2l = i < addr2line_results.size() ? addr2line_results[i].replace("/./", "/") : String("??");
+			print_error(vformat("[%s] %x - %s", String::num_int64(i).lpad(2), (uint64_t)bt_buffer[i], a2l));
 		}
-		args.push_back("-e");
-		args.push_back(_execpath);
-		args.push_back("-f");
-		args.push_back("-p");
-		args.push_back("-C");
-
-		// Try to get the file/line number using addr2line
-		String output;
-		Error err = OS::get_singleton()->execute(exe_name, args, &output, &ret);
-		if (err == OK) {
-			Vector<String> addr2line_results = output.substr(0, output.length() - 1).split("\n", false);
-
-			for (size_t i = 1; i < size; i++) {
-				// Simplify printed file paths to remove redundant `/./` sections (e.g. `/opt/godot/./core` -> `/opt/godot/core`).
-				print_error(vformat("[%d] %x - %s", (int64_t)i, (uint64_t)bt_buffer[i], addr2line_results[i].replace("/./", "/")));
-			}
-		} else {
-			// Otherwise fall back to trace symbols.
-			for (size_t i = 1; i < size; i++) {
-				char fname[1024];
-				Dl_info info;
-
-				snprintf(fname, 1024, "%s", strings[i]);
-
-				// Try to demangle the function name to provide a more readable one.
-				if (dladdr(bt_buffer[i], &info) && info.dli_sname) {
-					if (info.dli_sname[0] == '_') {
-						int status = 0;
-						char *demangled = abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
-
-						if (status == 0 && demangled) {
-							snprintf(fname, 1024, "%s", demangled);
-						}
-
-						if (demangled) {
-							free(demangled);
-						}
-					}
+	} else {
+		// Fall back to dladdr/cxxabi demangling when addr2line is unavailable.
+		for (int i = 1; i < size; i++) {
+			String symbol_name = i < symbols.size() ? symbols[i] : String("<unknown symbol>");
+			Dl_info info = {};
+			if (dladdr(bt_buffer[i], &info) != 0 && info.dli_sname != nullptr) {
+				const String demangled_name = StackTraceUnix::demangle_symbol_name(info.dli_sname);
+				if (!demangled_name.is_empty()) {
+					symbol_name = demangled_name;
 				}
-
-				print_error(vformat("[%d] %x - %s", (int64_t)i, (uint64_t)bt_buffer[i], fname));
 			}
+			print_error(vformat("[%s] %x - %s", String::num_int64(i).lpad(2), (uint64_t)bt_buffer[i], symbol_name));
 		}
-
-		free(strings);
 	}
 	print_error("-- END OF C++ BACKTRACE --");
 	print_error("================================================================");
